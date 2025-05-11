0068367db2f9e02734889b8ff707fdf8d488f938
Harsha Puranik <harshapuranik1704@gmail.com>
2025-05-11 14:18:08 -0400

    Updated global map memory

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -11,11 +11,27 @@ class MapMemoryNode : public rclcpp::Node {
   public:
     MapMemoryNode();
 
-    void listener(const nav_msgs::msg::Odometry::SharedPtr msg) const;
+    void costmapCallback(const nav_msgs::msg::Odometry::SharedPtr msg) const;
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) const;
+    void updateMap();
+    void integrateCostmap();
 
   private:
     robot::MapMemoryCore map_memory_;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odo_sub_;
+
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr map_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    // Global map and robot position
+    nav_msgs::msg::OccupancyGrid global_map_;
+    double last_x, last_y;
+    const double distance_threshold = 15;
+    nav_msgs::msg::OccupancyGrid latest_costmap_;
+
+    bool costmap_updated_ = false;
+    bool should_update_map_ = false;
 };
 
 #endif 
--- src/robot/map_memory/src/map_memory_node.cpp
+++ src/robot/map_memory/src/map_memory_node.cpp
@@ -1,15 +1,63 @@
+#include <chrono>
+
 #include "map_memory_node.hpp"
 
 MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())) {
-  odo_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom/filtered", 10, std::bind(&MapMemoryNode::listener, this, _1));
+  // Initialize subscribers
+  costmap_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+      "/costmap", 10, std::bind(&MapMemoryNode::costmapCallback, this, std::placeholders::_1));
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+      "/odom/filtered", 10, std::bind(&MapMemoryNode::odomCallback, this, std::placeholders::_1));
+
+  // Initialize publisher
+  map_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/map", 10);
+
+  // Initialize timer
+  timer_ = this->create_wall_timer(
+      std::chrono::seconds(1), std::bind(&MapMemoryNode::updateMap, this));
 }
 
-void MapMemoryNode::listener(const nav_msgs::msg::Odometry::SharedPtr msg) const {
+void MapMemoryNode::costmapCallback(const nav_msgs::msg::Odometry::SharedPtr msg) const {
+  RCLCPP_INFO(this->get_logger(), "RECEIVED NEW COSTMAP");
+  latest_costmap_ = *msg;
+  costmap_updated_ = true;
+}
+
+void MapMemoryNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) const {
   double x = msg->pose.pose.position.x;
   double y = msg->pose.pose.position.y;
-  RCLCPP_INFO(this->get_logger(), "Odometry x: '%f'", x);
-  RCLCPP_INFO(this->get_logger(), "Odometry y: '%f'", y);
+  RCLCPP_INFO(this->get_logger(), "Robot (x): '%f'", x);
+  RCLCPP_INFO(this->get_logger(), "Robot (y): '%f'", y);
+
+  // Compute distance traveled
+  double distance = std::sqrt(std::pow(x - last_x, 2) + std::pow(y - last_y, 2));
+  if (distance >= distance_threshold) {
+    last_x = x;
+    last_y = y;
+    should_update_map_ = true;
+  }
 }
+
+void MapMemoryNode::updateMap() {
+  if (should_update_map_ && costmap_updated_) {
+    integrateCostmap();
+    map_pub_->publish(global_map_);
+    should_update_map_ = false;
+    costmap_updated_ = false;
+  }
+}
+
+void MapMemoryNode::integrateCostmap() {
+  // Check if values in costmap are unknown - no change
+  
+
+  // Transform (scale) and merge the latest costmap into the global map for known cells
+  // by assigning a higher weight to new data
+
+  // Change global map headers? (could be done outside)
+  
+}
+
 
 int main(int argc, char ** argv)
 {
