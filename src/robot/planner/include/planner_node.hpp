ef3400ac96f5e86b7ae8cb49d52667f3affed5a2
Harsha Puranik <harshapuranik1704@gmail.com>
2025-05-13 21:31:51 -0400

    modify a* algo and refactor planner

--- src/robot/planner/include/planner_node.hpp
+++ src/robot/planner/include/planner_node.hpp
@@ -64,39 +64,39 @@ struct CompareF
   }
 };
 
+enum class State { 
+  WAITING_FOR_GOAL, 
+  WAITING_FOR_ROBOT_TO_REACH_GOAL
+};
+
 class PlannerNode : public rclcpp::Node {
   public:
     PlannerNode();
 
   private:
-    enum class State { WAITING_FOR_GOAL, WAITING_FOR_ROBOT_TO_REACH_GOAL };
     State state_{State::WAITING_FOR_GOAL};
   
-    /* ROS interfaces */
     rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_sub_;
     rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr goal_sub_;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
   
-    /* Data */
+    // data
     nav_msgs::msg::OccupancyGrid current_map_;
     geometry_msgs::msg::PointStamped goal_;
     geometry_msgs::msg::Pose robot_pose_;
-    bool goal_received_{false};
   
-    /* Callbacks / helpers ‑‑ prototypes only                               */
-    void mapCallback  (const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
-    void goalCallback (const geometry_msgs::msg::PointStamped::SharedPtr msg);
-    void odomCallback (const nav_msgs::msg::Odometry::SharedPtr msg);
+    void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    void goalCallback(const geometry_msgs::msg::PointStamped::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
     void timerCallback();
-
 
     bool goalReached();
     void planPath();
     bool worldToGrid(double wx, double wy, CellIndex& out) const;
     geometry_msgs::msg::PoseStamped gridToPose(const CellIndex& c) const;
-    bool isFree(int gx, int gy) const;
+    bool isFree(CellIndex& g) const;
     double heuristic(const CellIndex& a, const CellIndex& b) const;
 };
 
